933e5b6b25f2560638350f0f8c7e8a43910a58b6
abeerzahran <abeerzahram32@gmail.com>
2023-03-17 17:10:12 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26]="abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for(i=0 ; i<26 ; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}

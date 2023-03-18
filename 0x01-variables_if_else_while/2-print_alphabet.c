37ed78cfd18068c797d6019466bebe9d7899f1f3
abeerzahran <abeerzahram32@gmail.com>
2023-03-17 17:39:38 -0700

    done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-	char alph[26]="abcdefghijklmnopqrstuvwxyz";
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for(i=0 ; i<26 ; i++)
+
+	for (i = 0 ; i < 26 ; i++)
 	{
 		putchar(alph[i]);
 	}

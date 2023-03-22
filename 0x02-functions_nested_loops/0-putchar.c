a2b4ccc86443210933e024e0a34ca51748a6538f
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-03-23 01:26:48 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i = 0;
-	char st[]="_putchar";
+	char st[] = "_putchar";
 	
 	while (st[i] != '\0')
 	{
@@ -17,5 +17,6 @@ int main(void)
 		i++;
 	}
 	_putchar(10);
+	
 	return (0);
 }

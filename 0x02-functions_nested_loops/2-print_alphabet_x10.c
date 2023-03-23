20a43fb7926a053ddf43a71d2cd4bd41c4463076
abeerzahran <abeerzahram32@gmail.com>
2023-03-22 17:19:02 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabetx10 - main function
+ * Description: function to print alphabetx10
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 10;
+
+	while (num--)
+	{
+		print_alphabet();
+	}
+}

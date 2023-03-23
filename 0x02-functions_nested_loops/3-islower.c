3b8886fab8b80e2f12ece94fba30d83d7b9d699c
abeerzahran <abeerzahram32@gmail.com>
2023-03-22 17:53:03 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - main function
+ * Description: function is lower
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

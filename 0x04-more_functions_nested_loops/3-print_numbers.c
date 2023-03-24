a9f204f22438ef68b27e076c56811e108c62d833
abeerzahran <abeerzahram32@gmail.com>
2023-03-23 19:59:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch < '9'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}

eadd844e72b894beb34669c938451f7471275395
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-03-30 01:34:52 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,25 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count1 = 0, count2 = 0, c2 = 0;
+	int count = 0, count2 = 0;
 
-	while (*(dest + count1) != '\0')
+	while (*(dest + count) != '\0')
 	{
-		count1++;
+		count++;
 	}
-	while (*(src + c2) != '\0')
+
+	while (count2 < n)
 	{
-		c2++;
-	}
-	while (*(src + count2) != '\0')
-	{
-		*(dest + count1) = *(src + count2);
-		count1++;
-		count2++;
-		if (count2 >= n)
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
 			break;
+		count++;
+		count2++;
 	}
-	if (c2 < n)
-		*(dest + count1) = '\0';
 	return (dest);
 }

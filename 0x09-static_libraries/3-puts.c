cf8be92ef50d8221c10fd8bf036e368dcf2b9982
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-10 07:16:16 +0200

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,25 @@
 #include "main.h"
+
+void _puts(char *str)
+
+#include<stdio.h>
+
 /**
- * _puts - print char.
- * @s: char
- * Return: no return.
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
  */
-void _puts(char *s)
+
+int main(void)
+
 {
+
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
 
 }

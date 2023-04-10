adf1375e0e26fd2774c32cab6610aa6de91824b7
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-10 05:35:56 +0200

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _memcpy - a function.
+ * @dest: char
+ * @src:char
+ * @n: integer
+ * Return: return char.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 

e95511004cfbfa5ec88f698cddfabca373855d17
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-10 05:27:43 +0200

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _islower - main function
+ * _memset - main function
  * Description: function is lower
- * Return: 0
+ * Return: character
  */
 char *_memset(char *s, char b, unsigned int n)
 {

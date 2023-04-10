4f73c9ab15bd7f55e792c2ad61bcab4d01fb8798
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-10 05:44:16 +0200

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,3 +1,12 @@
 #include "main.h"
+/**
+ * _strncpy - the function.
+ * @dest: char
+ * @src: char
+ * @n: integer
+ * Return: return char.
+ */
 char *_strncpy(char *dest, char *src, int n)
-{}
+{
+
+}

f4971ecef8e331120ef621c8475880aba95c9314
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-10 05:38:41 +0200

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * find_sqrt - the function.
+ * @dest: char
+ * @src:char
+ * @n: integer
+ * Return: return char.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
+
 }

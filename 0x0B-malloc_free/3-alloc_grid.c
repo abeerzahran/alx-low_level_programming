dbec8bada135707a12cacba0fa22f0513d0ae971
abeerzahran <abeerzahram32@gmail.com>
2023-04-12 19:59:45 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * alloc_grid - concatinate a 2 strings
+ * @width: integer
+ * @height: integer
+ * Return: pointer or null
+ */
+int **alloc_grid(int width, int height)
+{
+	unsigned int i, a, b;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	arr = (int **) malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+		{
+			arr[a][b] = 0;
+		}
+	}
+	return (arr);
+}

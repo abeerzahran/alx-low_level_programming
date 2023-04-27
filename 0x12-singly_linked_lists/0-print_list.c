ac806104a71e672840fbc11d78f4807af69fcf8d
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-27 22:30:59 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,24 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	if (h->next != NULL)
+	{
+		size_t n;
 
-	n = 1;
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
+		n = 1;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] (%s)\n", h->len, h->str);
+		}
+		while (h->next == NULL)
+		{
+			return (n);
+		}
+		return (n + print_list(h->next));
 	}
-	else
-	{
-		printf("[%d] (%s)\n", h->len, h->str);
-	}
-	while (h->next == NULL)
-	{
-		return (n);
-	}
-	return (n + print_list(h->next));
+	return (0);
 }

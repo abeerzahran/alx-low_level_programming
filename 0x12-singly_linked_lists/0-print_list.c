951f123f7347e282defce61047ab9e97d1db23b0
abeerzahran <abeerzahran32@gmail.com>
2023-04-27 13:07:13 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print list
+ *
+ * @h: single linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 1;
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%lu] (%s)\n", h->len, h->str);
+	}
+	while (h->next == NULL)
+	{
+		return (n);
+	}
+	return (n + print_list(h->next));
+}

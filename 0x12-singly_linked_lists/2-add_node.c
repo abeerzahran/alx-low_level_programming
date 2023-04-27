ea4be5994d22cf8e999711fb0db41aea04426086
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-04-28 00:40:48 +0200

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - add node
+ * 
+ * @head: first node
+ * @str: string
+ * Return: address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t i;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if(new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (*head !=NULL)
+	{
+		new->next = *head;
+	}
+	else
+		new->next = NULL;
+		
+	new->str = strdup(str);
+		
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	*head = new;
+	new->len = i;
+
+	return (*head);
+}

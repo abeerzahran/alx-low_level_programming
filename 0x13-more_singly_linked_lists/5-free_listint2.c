938a1bc33fc01a2d9384b9208db7c04085a6f77d
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-01 03:51:08 +0200

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+  listint_t *temp = *head, *h = *head;
+  while (h != NULL)
+	{
+		temp = temp->next;
+		free(h);
+		h = temp;
+	}
+	*head = NULL;
+}

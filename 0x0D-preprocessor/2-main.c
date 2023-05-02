36095289e1e0e107e20a0d8a325f951c704c3d01
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-03 02:25:17 +0300

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

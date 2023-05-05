023c4e205299c9e6d7427c90a04858199464f6b4
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-05 04:38:50 +0300

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, power = 1, sum = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+	while (i--)
+	{
+		if (b[i] == '1')
+		{
+			sum += power;
+		}
+		power *= 2;
+	}
+	return (sum);
+}

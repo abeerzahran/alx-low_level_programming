40605ae4a5f4fcd2d339c03a8aa4303309816846
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-05 05:37:23 +0300

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int c = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (n)
+	{
+		if (c == index)
+		{
+			return (n & 1);
+		}
+		n = n >> 1;
+		c++;
+	}
+	return (-1);
+}

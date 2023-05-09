f87213fd1d3ff5ba7dbb15d72b9b35ee602d870e
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-09 05:51:03 +0300

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	int szw;
+	
+	if (!filename)
+		return (-1);
+	
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+
+		szw = write(fd, text_content, i);
+		if (szw == -1)
+		{
+			close(fd);
+		}
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}

16221fa4df9c26ac594c1e91dc81190b84ba2b22
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-09 05:54:37 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 	int szw;
-	
+
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 
 	if (fd == -1)
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 			close(fd);
 			return (-1);
 		}
-		
+
 	}
 	close(fd);
 	return (1);

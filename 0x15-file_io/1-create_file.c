6ec5db8c497a56ebe5e3388d51fe3744850ff54e
abeerzahran <106689767+abeerzahran@users.noreply.github.com>
2023-05-09 05:57:32 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,16 +23,16 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (!text_content)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
+		text_content = "";
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
 			;
 
-		szw = write(fd, text_content, i);
-		if (szw == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-
+	szw = write(fd, text_content, i);
+	if (szw == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
